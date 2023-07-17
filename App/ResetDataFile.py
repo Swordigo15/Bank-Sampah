f338e221d38d7870e83b959de050cccef938549f
Swordigo15 <67177319+Swordigo15@users.noreply.github.com>
2023-07-17 12:57:01 +0700

    Update ResetDataFile.py

--- App/ResetDataFile.py
+++ App/ResetDataFile.py
@@ -3,48 +3,44 @@ import pickle
 
 from datetime import datetime
 
-names = ["Parto", "Maria", "Mulyadi", "Siska", "Hestreni",
-            "Alex", "Ari", "Murni", "Rendi", "Atik", 
-            "Dinar", "Harti", "Heru", "Diah", "Astutiek", 
-            "Muchtar", "Elo"]
+names = ["PARTO", "MARIA", "MULYADI", "SISKA", "HESTRENI",
+            "ALEX", "ARI", "MURNI", "RENDI", "ATIK", 
+            "DINAR", "HARTI", "HERU", "DIAH", "ASTUTIEK", 
+            "MUCHTAR", "ELO"]
 
-List = [[0 for i in range(12)] for j in range(17)]
+List = [0] * 12
 
-for i in range(17):
-    List[i][0] = names[i]
-    print(List[i])
+data = {}
+
+# for i in range(17):
+#     data[names[i]] = List
+#     print(data[names[i]])
 
 file = open("Data.txt", "wb")
-pickle.dump(List, file)
+pickle.dump(data, file)
 file.close()
 
 print("")
 
 file = open("Data.txt", "rb")
 loadList = pickle.load(file)
-for i in range(17):
-    print(loadList[i])
+print(loadList)
+# for i in range(17):
+#     print(loadList[names[i]])
 file.close()
 
 now = datetime.now()
 mStr = int(now.strftime("%m"))
 yStr = int(now.strftime("%Y"))
 
-Data = {"Uang" : 0, "BP" : 0, "GP" : 0, 
-        "K" : 0, "D" : 0, "KK" : 0, 
-        "KH" : 0, "KB" : 0, "G" : 0, 
-        "BK" : 0, "MJ" : 0}
-
-#11x12
-
 mData = {}
 for i in range(12):
-    mData[i+1] = Data
+    mData[i+1] = List
 
 yData = {}
 yData[yStr] = mData
 
-print("yData[yStr][mStr] : \n", yData[yStr][mStr], "\n")
+# print("yData[yStr][mStr] : \n", yData[yStr][mStr], "\n")
 print("yData[yStr] : \n", yData[yStr], "\n")
 print("yData : \n", yData, "\n")
 
